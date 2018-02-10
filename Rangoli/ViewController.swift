989a4d0712f80c1d7ed3c2b708b6bf946f524f0b
Mangesh Gosavi <mangeshkhanderaogosavi@Mangeshs-Mac.local>
2018-02-10 12:54:04 +0530

    Initial Commit

---
+++ Rangoli/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rangoli
+//
+//  Created by Mangesh Khanderao Gosavi on 10/02/18.
+//  Copyright © 2018 Mangesh Khanderao Gosavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
